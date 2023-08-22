ed57761da638328498bbc6e4d23411fe72da56b5
RyanMathews <ryemathews@gmail.com>
2023-08-22 18:01:10 -0500

    new trinkets

---
+++ print_math.py
@@ -0,0 +1,16 @@
+from math import *
+from math import pi
+
+#Variable Definition
+
+f = 27*1.5
+w = 2*.025*sin((35*pi)/180)
+r = 27 * (2 ** (-5/3.8))
+p = ((5*8.314*415)/.27)/1000
+
+#Printing
+
+print("Force is", f, "N")
+print("Wavelength is", w, "nm")
+print("Radon-222 left is", r, "g")
+print("Pressure is", p, "kPA")
